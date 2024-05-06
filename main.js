0df824b02b2b3c1e05d15618fc7f4b2ac4438f49
michelegelmini <michelegelminivr@gmail.com>
2024-05-06 14:35:09 +0200

    add: basic-generation

---
+++ main.js
@@ -0,0 +1,7 @@
+const firstName = prompt('Inserisci il tuo nome');
+const lastName = prompt('Inserisci il tuo cognome');
+const color = prompt('Qual è il tuo colore preferito?');
+
+const output = `La tua password è ${firstName}${lastName}${color}${21}`
+
+console.log(output);
