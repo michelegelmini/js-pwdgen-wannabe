13b2695d5862d1de4a341944537292c8e5ce5b50
michelegelmini <michelegelminivr@gmail.com>
2024-05-06 14:38:47 +0200

    add: first-bonus

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const firstName = prompt('Inserisci il tuo nome');
 const lastName = prompt('Inserisci il tuo cognome');
 const color = prompt('Qual è il tuo colore preferito?');
+const firstNumber = prompt('Scegli un numero');
+const secondNumber = prompt('Scegli un secondo numero');
 
-const output = `La tua password è ${firstName}${lastName}${color}${21}`
+const output = `La tua password è ${firstName}${lastName}${color}${firstNumber / secondNumber}`
 
 console.log(output);
+
+document.getElementById('password-generated').innerHTML = output;
